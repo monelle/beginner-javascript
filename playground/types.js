612d26898ae1ea86ed92dbca87b3f132bcc04b97
Monelle Trachez <monelle.trachez@gmail.com>
2022-11-15 17:43:02 +0800

    Workfiles for basic lessons

---
+++ playground/types.js
@@ -0,0 +1,70 @@
+/* eslint-disable */  //command to disable eslint and automatic formatting
+
+//----------------------------------------------------
+// STRINGS
+/*
+console.log('it works');
+
+const myname = 'monelle';
+const middle = "lise";
+const last = `trachez`;
+
+const sentence ='she\'s so cool'; // back slash allows that apostrophe to work as such, not a single quote
+const sentence2 = "she's so \"cool\"";
+
+const song = `Too late
+You wanna make it right, but now it's too late
+My peanut butter chocolate cake with Kool-Aid
+I'm trying not to waste my time`
+
+console.log(song);
+
+// Concatenation vs. Interpolation
+// Concatenation: 2 or more strings combined into one using '+' as the operator
+// Conc example with single and double-quote string:
+const hello = "hello my name is " + myname + ". Nice meeting you."
+console.log(hello);
+
+// Interpolation: a variable is inserted inside a string.
+// this ${} can only be used with back ticks
+const hello2 = `Hello again this time my name is ${myname}. Good to see you again. I'm ${20+24} years old.`
+console.log(hello2);
+
+const html = `
+    <div>
+        <h2>${myname}</h2>
+        <p>${hello}</p>
+    </div>
+`;
+// console.log(html);
+document.body.innerHTML = html;
+*/
+
+
+//----------------------------------------------------
+// NUMBERS
+/*
+//Using helper method + modulo:
+const smarties = 20;
+const kids = 3;
+const eachKidGets = Math.floor(smarties / kids);
+const moGets = smarties % kids 
+
+console.log(`Each one gets ${eachKidGets}.`);
+console.log(`Monelle gets ${moGets}.`);
+*/
+
+
+//----------------------------------------------------
+// OBJECTS
+/*
+const person = {
+    first: `monelle`,
+    last: `trachez`,
+    age: 44
+};
+*/
+
+
+//----------------------------------------------------
+// NULL and UNDEFINED
